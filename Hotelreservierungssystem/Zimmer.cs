06403d9a1b7ad37c8b01316851ec388168f5fc2e
medofrh <medo0o0frh@gmail.com>
2024-04-25 17:42:40 +0200

    Klasse Zimmer.cs

---
+++ Hotelreservierungssystem/Zimmer.cs
@@ -0,0 +1,33 @@
+using System.Runtime.InteropServices;
+
+namespace Hotelreservierungssystem;
+
+public class Zimmer
+{
+    public int ZimmerNr;
+    public enum ZimmerTyp  
+    {
+        Einzelzimmer,
+        Doppelzimmer,
+        Suite
+    }
+
+    public decimal PreisProNacht;
+    public bool IstBelegt;
+    
+    public Zimmer(int zimmerNr, ZimmerTyp zimmerTyp, decimal preisProNacht)
+    {
+        ZimmerNr = zimmerNr;
+        PreisProNacht = preisProNacht;
+    }
+    
+    public void Reservieren()
+    {
+        IstBelegt = true;
+    }
+    
+    public void Stornieren()
+    {
+        IstBelegt = false;
+    }
+}
